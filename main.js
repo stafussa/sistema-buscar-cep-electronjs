6aafde5aa5739fbfc17cc94bc4f42c563da3968d
douglas ferreira <douglasstafussa15@gmail.com>
2024-05-15 22:03:29 -0300

    att

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
-    
+    icon: './imagem/image.png',
     })
   
     win.loadFile('index.html')
