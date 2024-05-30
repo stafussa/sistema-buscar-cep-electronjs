4a27b1eb692acdfdc91d13b4ad4d7d04b4f9b64b
douglas ferreira <douglasstafussa15@gmail.com>
2024-05-29 22:11:13 -0300

    att do app de cep

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 var formulario = document.getElementById('formulario');
 var CEP = document.getElementById('CEP');
 
+var tbody = document.getElementById('tbody');
+
+
+
+//função para mostrar o CEP no console
+var listadeCeps = [];
+
 //função responsavel por buscar o CEP
 function buscarCep(event) {
     //previne o comportamento padrao do formulário
@@ -22,30 +29,50 @@ function buscarCep(event) {
         .then(data => {
             console.log(data);
 
+            listadeCeps.push(data);
+
+            console.log(listadeCeps);
+
+            var novaLinha = tbody.insertRow();
+
+            var celulaCep = novaLinha.insertCell(0);
+            var celulaLogradouro = novaLinha.insertCell(1);
+            var celulaBairro = novaLinha.insertCell(2);
+            var celulaCidade = novaLinha.insertCell(3);
+            var celulaUf = novaLinha.insertCell(4);
+
+            listadeCeps.forEach(item => {
+                celulaCep.innerText = item.cep
+                celulaLogradouro.innerText = item.logradouro ? item.logradouro : '-----'
+                celulaBairro.innerText = item.bairro ? item.bairro : '-----'
+                celulaCidade.innerText = item.localidade
+                celulaUf.innerText = item.uf
+            })
+            
+          
+
             //pegando o elemento do hatml pelo id
             var resultado = document.getElementById('Resultado');
 
             //adicionando o valor no html
             resultado.innerText = `CEP: ${data.cep} - ${data.logradouro} , ${data.bairro}, ${data.localidade} - ${data.uf}`;
+
         })
 
- 
+
 }
 
 function mascaraCep(event) {
 
-event.currentTarget.maxLength = 9
+    event.currentTarget.maxLength = 9
     let value = event.currentTarget.value
     value = value.replace(/\D/g, '')
     value = value.replace(/^(\d{5})(\d)/, '$1-$2')
     event.currentTarget.value = value
-    return e 
+    return event
 }
 
-CEP.addEventListener("keyup",mascaraCep)
-
-
-
+CEP.addEventListener("keyup", mascaraCep)
 
 //adicionando um evento de (evento) ao formulário
 formulario.addEventListener('submit', buscarCep);
